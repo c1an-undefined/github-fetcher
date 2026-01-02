2ac60f0ae043f60f2f34ff177fe7af7629d29483
ReeperC3 <cian2neiland@gmail.com>
2026-01-02 11:28:21 +0000

    Successfully receives data from github

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// https://api.github.com/users/USERNAME
+const fetchBtn = document.getElementById("fetch-profile-btn")
+const usernameInput = document.getElementById("username-input")
+
+async function fetchUser(username) {
+    const url = `https://api.github.com/users/${username}`
+    try {
+        const response = await fetch(url)
+        const result = await response.json()
+        console.log(result)
+    }
+    catch (error) {
+        console.error(error.message)
+    }
+}
+
+fetchBtn.addEventListener("click", ()=> {
+    fetchUser(usernameInput.value)
+})
